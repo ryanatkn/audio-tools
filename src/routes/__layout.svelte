f1cd16285e277eed136cc0e7c14e209643a91034
Ryan Atkinson <mail@ryanatkn.com>
2021-07-17 19:03:53 -0500

    upgrade sveltekit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,7 @@
 <script>
-	// SvelteKit/Vite behavior makes this to look out of order, is reverse-cascade
-	import '$lib/ui/style.css'; // overrides for the next import
-	import '@feltcoop/felt/ui/style.css'; // overrode by previous import
+	import '@feltcoop/felt/ui/style.css';
+
+	import '$lib/ui/style.css';
 </script>
 
 <svelte:head>
